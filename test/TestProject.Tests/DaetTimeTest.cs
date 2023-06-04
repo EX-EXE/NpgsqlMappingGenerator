6039e2ac57ddafaee6a83a1ff06984cf7dffa2a9
EXE <expexe.jp@gmail.com>
2023-06-04 16:23:19 +0900

    Test

---
+++ test/TestProject.Tests/DaetTimeTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using System;
+using Xunit.Abstractions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestProject.Tests;
+
+public class DateTimeTest : PrepareDataBase, IAsyncLifetime
+{
+    public DateTimeTest(ITestOutputHelper outputHelper)
+        : base(outputHelper, nameof(DateTimeTest))
+    {
+    }
+
+    public async Task InitializeAsync()
+    {
+        await DateTimeTable.CreateTableAsync(Connection).ConfigureAwait(false);
+    }
+
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task Test()
+    {
+        var dateTime = new DateTime(0, 1, 2, 3, 4, 5, DateTimeKind.Local);
+        var dateTimeUtc = new DateTime(6, 7, 8, 9, 10, 11, DateTimeKind.Utc);
+        var dateTimeOffset = new DateTimeOffset(12, 13, 14, 15, 16, 17, TimeSpan.FromHours(9.0));
+
+        OutputHelper.WriteLine($"Insert {nameof(dateTime)}:{dateTime}({dateTime.Kind})");
+        OutputHelper.WriteLine($"Insert {nameof(dateTimeUtc)}:{dateTimeUtc}({dateTime.Kind})");
+        OutputHelper.WriteLine($"Insert {nameof(dateTimeOffset)}:{dateTimeOffset}");
+
+        await DateTimeTable.InsertAsync(Connection,
+            new DateTimeTable.IDbParam[]
+            {
+                new DateTimeTable.DbParamDateTime(dateTime),
+                new DateTimeTable.DbParamDateTimeUtc(dateTimeUtc),
+                new DateTimeTable.DbParamDateTimeOffset(dateTimeOffset),
+            }).ConfigureAwait(false);
+
+        await foreach (var row in DateTimeTable.SelectAsync(Connection, DateTimeTable.DbColumnType.All))
+        {
+            OutputHelper.WriteLine($"Select DateTime:{row.DateTime}({row.DateTime.Kind})");
+            OutputHelper.WriteLine($"Select DateTimeUtc:{row.DateTimeUtc}({row.DateTimeUtc.Kind})");
+            OutputHelper.WriteLine($"Select DateTimeOffset:{row.DateTimeOffset}");
+            row.DateTime.Should().Be(dateTime);
+            row.DateTimeUtc.Should().Be(dateTimeUtc);
+            row.DateTimeOffset.Should().Be(dateTimeOffset);
+        }
+    }
+}
---
+++ test/TestProject/DateTimeTable.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+using NpgsqlMappingGenerator;
+
+namespace TestProject;
+
+
+[DbTableGenerator("public.datetime_table")]
+public partial class DateTimeTable
+{
+    [DbColumn<DbParamGuid>("id")]
+    [DbAutoCreate<DbAutoCreateGuid>(DbAutoCreateType.Insert)]
+    public Guid Id { get; set; }
+
+    [DbColumn<DbParamDateTime>("datetime")]
+    public DateTime DateTime { get; set; } = DateTime.Now;
+
+    [DbColumn<DbParamDateTimeUtc>("datetime_utc")]
+    public DateTime DateTimeUtc { get; set; } = DateTime.UtcNow;
+
+    [DbColumn<DbParamDateTimeOffset>("datetimeoffset")]
+    public DateTimeOffset DateTimeOffset { get; set; } = DateTime.UtcNow;
+
+    public static async ValueTask CreateTableAsync(NpgsqlConnection connection)
+    {
+        using var cmd = new NpgsqlCommand("create table public.datetime_table (id uuid not null ,datetime timestamp without time zone not null ,datetime_utc timestamp with time zone not null ,datetimeoffset timestamp with time zone not null  ,primary key (id) );", connection);
+        await cmd.PrepareAsync().ConfigureAwait(false);
+        await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+    }
+}
