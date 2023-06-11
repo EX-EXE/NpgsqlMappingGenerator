82a6c3726624b70dca8d1b56f4c21752e69de1f7
EXE <expexe.jp@gmail.com>
2023-06-11 13:52:19 +0900

    Add Test.

---
+++ test/TestProject.Tests/Table/StringTest.cs
@@ -0,0 +1,85 @@
+﻿using FluentAssertions;
+using System;
+using Xunit.Abstractions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestProject.Tests.Table;
+
+public class StringTest : PrepareDataBase, IAsyncLifetime
+{
+    public StringTest(ITestOutputHelper outputHelper)
+        : base(outputHelper, nameof(StringTest))
+    {
+    }
+
+    public async Task InitializeAsync()
+    {
+        await StringTable.CreateTableAsync(Connection).ConfigureAwait(false);
+        await StringTable.InsertAsync(Connection,
+            new StringTable.IDbParam[]
+            {
+                new StringTable.DbParamData("TestData"),
+            }).ConfigureAwait(false);
+    }
+
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task LikeTest()
+    {
+        bool hit = false;
+        await foreach (var row in StringTable.SelectAsync(
+            Connection,
+            StringTable.DbQueryType.All,
+            where: new StringTable.DbCondition(NpgsqlMappingGenerator.DbCompareOperator.Like, new StringTable.DbParamData("TestData"))))
+        {
+            hit |= true;
+        }
+        hit.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task LikeTest2()
+    {
+        bool hit = false;
+        await foreach (var row in StringTable.SelectAsync(
+            Connection,
+            StringTable.DbQueryType.All,
+            where: new StringTable.DbCondition(NpgsqlMappingGenerator.DbCompareOperator.Like, new StringTable.DbParamData("testData"))))
+        {
+            hit |= true;
+        }
+        hit.Should().BeFalse();
+    }
+    [Fact]
+    public async Task ILikeTest()
+    {
+        bool hit = false;
+        await foreach (var row in StringTable.SelectAsync(
+            Connection,
+            StringTable.DbQueryType.All,
+            where: new StringTable.DbCondition(NpgsqlMappingGenerator.DbCompareOperator.ILike, new StringTable.DbParamData("TestData"))))
+        {
+            hit |= true;
+        }
+        hit.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ILikeTest2()
+    {
+        bool hit = false;
+        await foreach (var row in StringTable.SelectAsync(
+            Connection,
+            StringTable.DbQueryType.All,
+            where: new StringTable.DbCondition(NpgsqlMappingGenerator.DbCompareOperator.ILike, new StringTable.DbParamData("testData"))))
+        {
+            hit |= true;
+        }
+        hit.Should().BeTrue();
+    }
+}
---
+++ test/TestProject/Table/StringTable.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+using NpgsqlMappingGenerator;
+
+namespace TestProject;
+
+
+[DbTableGenerator("public.string_table")]
+public partial class StringTable
+{
+    [DbColumn<DbParamGuid>("id")]
+    [DbAutoCreate<DbAutoCreateGuid>(DbAutoCreateType.Insert)]
+    public Guid Id { get; set; }
+
+    [DbColumn<DbParamDateTime>("data")]
+    public string Data { get; set; } = string.Empty;
+
+    public static async ValueTask CreateTableAsync(NpgsqlConnection connection)
+    {
+        using var cmd = new NpgsqlCommand($"create table {DbTableQuery} (id uuid not null ,data text not null ,primary key (id) );", connection);
+        await cmd.PrepareAsync().ConfigureAwait(false);
+        await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+    }
+}
