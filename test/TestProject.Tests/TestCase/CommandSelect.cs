ff121411f77df94aac38c1fc7a121157bdf66886
EXE <expexe.jp@gmail.com>
2023-10-28 19:59:51 +0900

    Add Test.

---
+++ test/TestProject.Tests/TestCase/CommandSelect.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using FluentAssertions;
+using TestProject.TestCase;
+
+namespace TestProject.Tests.TestCase;
+
+public class CommandSelect : PrepareDataBase, IAsyncLifetime
+{
+    List<UserData> UserList = new();
+    List<AuthorityType> AuthorityTypeList = new();
+    List<UserAuthority> UserAuthorityList = new();
+
+    public CommandSelect(ITestOutputHelper outputHelper)
+        : base(outputHelper, nameof(ViewSelect))
+    {
+    }
+
+    public async Task InitializeAsync()
+    {
+        await UserData.CreateTableAsync(Connection).ConfigureAwait(false);
+        await AuthorityType.CreateTableAsync(Connection).ConfigureAwait(false);
+        await UserAuthority.CreateTableAsync(Connection).ConfigureAwait(false);
+
+        var rand = new Random();
+
+        foreach (var num in Enumerable.Range(0, rand.Next(10, 100)))
+        {
+            var userData = new UserData()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = $"First{num}",
+                LastName = $"Last{num}",
+            };
+            UserList.Add(userData);
+
+            await UserData.InsertAsync(Connection,
+                new UserData.IDbParam[]
+                {
+                    new UserData.DbParamId(userData.Id),
+                    new UserData.DbParamFirstName(userData.FirstName),
+                    new UserData.DbParamLastName(userData.LastName),
+                }).ConfigureAwait(false);
+        }
+        foreach (var num in Enumerable.Range(0, rand.Next(5, 20)))
+        {
+            var type = new AuthorityType()
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Name{num}",
+            };
+            AuthorityTypeList.Add(type);
+
+            await AuthorityType.InsertAsync(Connection,
+                new AuthorityType.IDbParam[]
+                {
+                    new AuthorityType.DbParamId(type.Id),
+                    new AuthorityType.DbParamName(type.Name),
+                }).ConfigureAwait(false);
+        }
+        foreach (var user in UserList)
+        {
+            foreach (var type in AuthorityTypeList.Take(rand.Next(0, AuthorityTypeList.Count - 1)))
+            {
+                var userType = new UserAuthority()
+                {
+                    UserDataId = user.Id,
+                    AuthorityTypeId = type.Id,
+                    LastUpdate = DateTime.Now,
+                };
+                UserAuthorityList.Add(userType);
+
+                await UserAuthority.InsertAsync(Connection,
+                    new UserAuthority.IDbParam[]
+                    {
+                    new UserAuthority.DbParamUserDataId(user.Id),
+                    new UserAuthority.DbParamAuthorityTypeId(type.Id),
+                    }).ConfigureAwait(false);
+            }
+        }
+    }
+
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task Select()
+    {
+        await foreach (var row in ViewUserAuthority.ExecuteAsync(Connection, ViewUserAuthority.DbQueryType.All))
+        {
+            var userData = UserList.First(x => x.Id == row.UserId);
+            var typeData = AuthorityTypeList.First(x => x.Id == row.AuthorityId);
+            userData.FirstName.Should().Be(row.FirstName);
+            userData.LastName.Should().Be(row.LastName);
+            typeData.Name.Should().Be(row.AuthorityName);
+        }
+    }
+}
---
+++ test/TestProject/TestCase/ViewUserAuthority.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NpgsqlMappingGenerator;
+
+namespace TestProject.TestCase
+{
+    [DbCommand(
+        "SELECT public.userdata.id as userid, public.userdata.first_name as first_name, public.userdata.last_name as last_name, public.authority_type.name as authority_name , public.authority_type.id as authorityid " +
+        "FROM public.user_authority " +
+        "JOIN public.userdata ON public.userdata.id = public.user_authority.user_data_id " +
+        "JOIN public.authority_type ON public.authority_type.id = public.user_authority.authority_type_id ")]
+    public partial class ViewUserAuthority
+    {
+        [DbColumn<DbParamGuid>("userid")]
+        public Guid UserId { get; set; }
+
+        [DbColumn<DbParamGuid>("authorityid")]
+        public Guid AuthorityId { get; set; }
+
+        [DbColumn<DbParamString>("first_name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [DbColumn<DbParamString>("last_name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [DbColumn<DbParamString>("authority_name")]
+        public string AuthorityName { get; set; } = string.Empty;
+    }
+}
