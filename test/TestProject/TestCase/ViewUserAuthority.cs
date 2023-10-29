912fee005310d978898a101bc455d66af596cedd
EXE <expexe.jp@gmail.com>
2023-10-29 09:40:17 +0900

    Fix Command.

--- test/TestProject/TestCase/ViewUserAuthority.cs
+++ test/TestProject/TestCase/ViewUserAuthority.cs
@@ -33,7 +33,7 @@ namespace TestProject.TestCase
     [DbCommand(
         "SELECT public.userdata.id as userid, public.userdata.first_name as first_name, public.userdata.last_name as last_name, public.authority_type.name as authority_name , public.authority_type.id as authorityid " +
         "FROM public.user_authority " +
-        "JOIN public.userdata ON public.userdata.id = public.user_authority.user_data_id AND public.userdata.id = @param_userid" +
+        "JOIN public.userdata ON public.userdata.id = public.user_authority.user_data_id AND public.userdata.id = @param_userid " +
         "JOIN public.authority_type ON public.authority_type.id = public.user_authority.authority_type_id ")]
     [DbCommandParam<DbParamGuid>("@param_userid")]
     public partial class ViewUserAuthorityOn
