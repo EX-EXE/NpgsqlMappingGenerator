c4d44326ca206323bd5994e4c1acee615d389950
EXE <expexe.jp@gmail.com>
2023-11-14 20:16:14 +0900

    Test

--- test/TestProject/TestCase/ViewUserAuthority.cs
+++ test/TestProject/TestCase/ViewUserAuthority.cs
@@ -31,11 +31,12 @@ JOIN public.authority_type ON public.authority_type.id = public.user_authority.a
         public string AuthorityName { get; set; } = string.Empty;
     }
 
-    [DbCommand(
-        "SELECT public.userdata.id as userid, public.userdata.first_name as first_name, public.userdata.last_name as last_name, public.authority_type.name as authority_name , public.authority_type.id as authorityid " +
-        "FROM public.user_authority " +
-        "JOIN public.userdata ON public.userdata.id = public.user_authority.user_data_id AND public.userdata.id = @param_userid " +
-        "JOIN public.authority_type ON public.authority_type.id = public.user_authority.authority_type_id ")]
+    [DbCommand("""
+SELECT public.userdata.id as userid, public.userdata.first_name as first_name, public.userdata.last_name as last_name, public.authority_type.name as authority_name , public.authority_type.id as authorityid
+FROM public.user_authority
+JOIN public.userdata ON public.userdata.id = public.user_authority.user_data_id AND public.userdata.id = @param_userid
+JOIN public.authority_type ON public.authority_type.id = public.user_authority.authority_type_id
+""")]
     [DbCommandParam<DbParamGuid>("@param_userid")]
     public partial class ViewUserAuthorityOn
     {
